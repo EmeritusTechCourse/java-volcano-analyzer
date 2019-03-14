899bee0a08c1157c1644305a3504257a1c4eebe1
Jeffrey Klier <jeffrey.klier@galvanize.com>
2019-03-14 13:46:39 -0700

    added solutions

--- src/main/java/VolcanoAnalyzer.java
+++ src/main/java/VolcanoAnalyzer.java
@@ -3,8 +3,11 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
@@ -31,4 +34,51 @@ public class VolcanoAnalyzer {
 
     //add methods here to meet the requirements in README.md
 
+    public List<Volcano> eruptedInEighties() {
+        return volcanos.parallelStream().filter(v -> v.getYear() >= 1980 && v.getYear() < 1990).collect(Collectors.toList());
+    }
+
+    public String[] highVEI() {
+        return volcanos.parallelStream().filter(v -> v.getVEI() >= 6).map(v -> v.getName()).collect(Collectors.toList()).toArray(new String[0]);
+    }
+
+    public Volcano mostDeadly() {
+        return volcanos.stream().reduce(volcanos.get(0), (v, acc) -> Integer.parseInt(v.getDEATHS() == "" ? "0" : v.getDEATHS()) > Integer.parseInt(acc.getDEATHS() == "" ? "0" : acc.getDEATHS()) ? v : acc);
+    }
+
+    public double causedTsunami() {
+        return ((double)volcanos.parallelStream().filter(v -> v.getTsu().equals("tsu")).collect(Collectors.toList()).size()) * 100 / volcanos.size() ;
+    }
+
+    public String mostCommonType() {
+        return volcanos.stream().collect(Collectors.collectingAndThen(Collectors.groupingBy(Volcano::getType, Collectors.counting()), map -> map.entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey()));
+    }
+
+    public Integer eruptionsByCountry(String country) {
+        return volcanos.stream().filter(v -> v.getCountry().equals(country)).collect(Collectors.toList()).size();
+    }
+
+    public double averageElevation() {
+        return (double)volcanos.stream().collect(Collectors.summingInt(Volcano::getElevation))/volcanos.size();
+    }
+
+    public String[] volcanoTypes() {
+        return volcanos.stream().map(v-> v.getType()).distinct().collect(Collectors.toList()).toArray(new String[0]);
+    }
+
+    public double percentNorth() {
+        return ((double)volcanos.parallelStream().filter(v -> v.getLatitude() > 0).collect(Collectors.toList()).size()) * 100 /volcanos.size();
+    }
+
+    public String[] manyFilters() {
+        return volcanos.stream().filter(v -> v.getYear() > 1800).filter(v -> v.getTsu() == "").filter(v -> v.getLatitude() < 0 ).filter(v -> v.getVEI() == 5).map(v -> v.getName()).collect(Collectors.toList()).toArray(new String[0]);
+    }
+
+    public String[] elevatedVolcanoes(int elevation) {
+        return volcanos.stream().filter(v -> v.getElevation() >= elevation).map(v->v.getName()).collect(Collectors.toList()).toArray(new String[0]);
+    }
+
+    public String[] topAgentsOfDeath() {
+        return volcanos.stream().sorted((v1, v2)->Integer.parseInt("0" + v2.getDEATHS()) - (Integer.parseInt("0" + v1.getDEATHS()))).limit(10).map(v-> Arrays.asList( v.getAgent().isEmpty() ? new String[0] : v.getAgent().split(","))).flatMap(List::stream).distinct().collect(Collectors.toList()).toArray(new String[0]);
+    }
 }
